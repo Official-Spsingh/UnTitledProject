455746d377e2417f5595c39a6cf3ca598fe73124
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2022-05-07 13:59:29 +0530

    initialized project in react 18

--- untitledproject/src/routes/index.jsx
+++ untitledproject/src/routes/index.jsx
@@ -4,12 +4,13 @@ import Landing from '../components/Landing'
 import NavBar from '../components/NavBar'
 import NextPage from '../components/NextPage';
 const RoutesComponent = (props) => {
+    const { settheme, ...otherProps } = props
     return (
         <BrowserRouter>
             <NavBar {...props} />
             <Routes>
-                <Route path="/" element={<Landing {...props} />} />
-                <Route path="nextpage" element={<NextPage {...props} />} />
+                <Route path="/" element={<Landing {...otherProps} />} />
+                <Route path="nextpage" element={<NextPage {...otherProps} />} />
                 <Route path="*" element={<div>Not Found</div>} />
             </Routes>
         </BrowserRouter>
